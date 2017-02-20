cac34f09a61d53a3812a80f058fab137a22d5b72
aquirozj <anahiqj@ciencias.unam.mx>
2017-02-20 00:28:05 -0600

    Update practica1.c

--- practica1.c
+++ practica1.c
@@ -3,26 +3,38 @@
 #include <string.h>
 
 void mi_printf( char *str1, char *str2){
+
+	int i= 0;
+	int j= 0;
+	while (str1[i] != '\0'){
 		
+		if(str1[i]== '%' && str1[i+1]=='s'){
+			printf(" %s ",str2);
+			i++;
+		}else{
+			if(str1[i]=='s'){
+				i++;			
+			}else{
+				printf("%c",str1[i] );
+				i++;
+			}
+		}
+	}
+
+	free(str1);
+	free(str2);ls
+	
 }
 
 int main(){
-	char *cad1= (char*)malloc(50*sizeof(char));
-	char *cad2= (char*)malloc(20*sizeof(char));
+	char *str1= (char*)malloc(20);
+	char *str2= (char*)malloc(20);
 
 	printf("Escribe la cadena que se desplegara en pantalla\n ");
-	scanf("%s", &cad1);
-
-	int i;
-	for (i=0;i<50; i++){
-		printf("%s ", *cad1+1);
-	}
-	printf("Escribe la segunda cadena a reemplazar por el simbolo\n");
-	scanf("%s", &cad2);		
-		
-
-
-//	free(cad1);
-//	free(cad2);
+	scanf("%s", str1);
+	printf("Escribe la segunda cadena a reemplazar\n");
+	scanf("%s", str2);		
+	mi_printf(str1, str2);
+	printf("\n");
 	return 0;
 }
